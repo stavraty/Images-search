42d30c178a2945b988c9b935b642ac22241cdaf6
Stavratiy Andriy <stavraty@gmail.com>
2023-07-25 12:34:55 +0300

    Download button

--- Images search/Controller/ImagePageVC.swift
+++ Images search/Controller/ImagePageVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Photos
 
 class ImagePageVC: BaseVC {
     
@@ -14,6 +15,7 @@ class ImagePageVC: BaseVC {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var zoomButton: UIButton!
     @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var photoFormatLabel: UILabel!
     
     var largeImageURL: URL?
     private let imageCacheService = ImageCacheService.shared
@@ -21,14 +23,10 @@ class ImagePageVC: BaseVC {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupActivityIndicator()
-        
-        zoomButton.setTitle("", for: .normal)
-        shareButton.layer.cornerRadius = 5
-        shareButton.clipsToBounds = true
-        shareButton.layer.borderWidth = 1.0
-        shareButton.layer.borderColor = UIColor(red: 0.26, green: 0.04, blue: 0.88, alpha: 1.00).cgColor
+        setupButtons()
+        updatePhotoFormatLabel()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
@@ -38,7 +36,6 @@ class ImagePageVC: BaseVC {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showImageZoomViewSegue" {
             if let destinationVC = segue.destination as? ImageZoomVC {
-                // destinationVC.zoomedImage = selectedImage.image
                 destinationVC.largeImageURL = largeImageURL
             }
         }
@@ -47,6 +44,14 @@ class ImagePageVC: BaseVC {
     private func setupActivityIndicator() {
         activityIndicator.isHidden = true
         activityIndicator.hidesWhenStopped = true
+    }
+    
+    private func setupButtons() {
+        zoomButton.setTitle("", for: .normal)
+        shareButton.layer.cornerRadius = 5
+        shareButton.clipsToBounds = true
+        shareButton.layer.borderWidth = 1.0
+        shareButton.layer.borderColor = UIColor(red: 0.26, green: 0.04, blue: 0.88, alpha: 1.00).cgColor
     }
     
     private func loadLargeImage() {
@@ -72,9 +77,68 @@ class ImagePageVC: BaseVC {
         }
     }
     
+    private func getImageFormat(from url: URL) -> String? {
+        let pathExtension = url.pathExtension.lowercased()
+        switch pathExtension {
+            case "jpg", "jpeg":
+                return "JPG"
+            case "png":
+                return "PNG"
+            default:
+                return nil
+        }
+    }
+    
+    private func updatePhotoFormatLabel() {
+        guard let imageURL = largeImageURL, let format = getImageFormat(from: imageURL) else {
+            return
+        }
+        
+        let labelText = "Photo in .\(format) format"
+        photoFormatLabel.text = labelText
+    }
+    
+    private func downloadAndSaveImage(from imageURL: URL) {
+        URLSession.shared.dataTask(with: imageURL) { [weak self] (data, response, error) in
+            DispatchQueue.main.async {
+
+                if let error = error {
+                    print("Failed to download the image: \(error)")
+                    return
+                }
+
+                guard let data = data, let image = UIImage(data: data) else {
+                    print("Invalid image data")
+                    return
+                }
+
+                PHPhotoLibrary.shared().performChanges({
+                    PHAssetChangeRequest.creationRequestForAsset(from: image)
+                }) { [weak self] success, error in
+                    if success {
+                        DispatchQueue.main.async {
+                            self?.showAlert(title: "Success", message: "Image saved to the gallery.")
+                        }
+                    } else {
+                        print("Failed to save the image: \(error?.localizedDescription ?? "Unknown error")")
+                    }
+                }
+            }
+        }.resume()
+    }
+
+    private func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+
     @IBAction func zoomButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "showImageZoomViewSegue", sender: nil)
     }
+    
     @IBAction func shareButtonTapped(_ sender: Any) {
         guard let imageURL = largeImageURL else {
             return
@@ -86,4 +150,11 @@ class ImagePageVC: BaseVC {
         }
     }
     
+    @IBAction func downloadButtonTapped(_ sender: Any) {
+        guard let imageURL = largeImageURL else {
+            return
+        }
+
+        downloadAndSaveImage(from: imageURL)
+    }
 }
